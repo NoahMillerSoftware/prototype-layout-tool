713933dad17d89dc2218f907de3145af592b3ff6
Noah Miller <nmiller@drw.com>
2018-10-03 08:38:46 -0500

    Added 'required=True' to --spec and --out command line options

--- scripts/plt.py
+++ scripts/plt.py
@@ -6,12 +6,11 @@ from prototype_layout_tool.core import read_specs, draw_cards
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Game Prototype Layout")
-    parser.add_argument('-s', '--spec', dest='spec_filename')
-    parser.add_argument('-o', '--out', dest='out_filename')
+    parser.add_argument('-s', '--spec',
+                        dest='spec_filename', required=True)
+    parser.add_argument('-o', '--out',
+                        dest='out_filename', required=True)
     args = parser.parse_args()
 
-    if not args.spec_filename or not args.out_filename:
-        parser.print_help()
-    else:
-        (card_list, layout) = read_specs(args.spec_filename)
-        draw_cards(card_list, args.out_filename, layout)
+    (card_list, layout) = read_specs(args.spec_filename)
+    draw_cards(card_list, args.out_filename, layout)
