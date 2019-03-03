469bed6cf7a52c3d03cc13b5e57444d9230062cc
Noah Miller <nmiller@DRWHoldings.com>
2019-03-02 19:22:31 -0600

    Added ICF Oblique layout and page size Added support for any installed TTF fonts

--- prototype_layout_tool/core.py
+++ prototype_layout_tool/core.py
@@ -6,19 +6,22 @@ from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 from reportlab.platypus import Frame, Paragraph, Image
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
-styles = getSampleStyleSheet()
+# anchors are center of each card
 
-# anchors are center of of each card
 # 3x3 poker cards on 8.5"x11" paper
-POKER_9 = {'card_dim' : (2.5*inch, 3.5*inch),
+POKER_9 = {'page' : letter,
+           'card_dim' : (2.5*inch, 3.5*inch),
            'anchors' : [(1.75, 9)  , (4.25, 9)  , (6.75, 9),
                         (1.75, 5.5), (4.25, 5.5), (6.75, 5.5),
                         (1.75, 2)  , (4.25, 2)  , (6.75, 2)],
            'card_rots' : [0,0,0,0,0,0,0,0,0]}
 POKER_9['anchors'] = [(inch*a[0], inch*a[1]) for a in POKER_9['anchors']]
 
-POKER_L = {'card_dim' : (3.5*inch, 2.5*inch),
+POKER_L = {'page' : letter,
+           'card_dim' : (3.5*inch, 2.5*inch),
            'anchors' : [(1.75, 9)  , (4.25, 9)  , (6.75, 9),
                         (1.75, 5.5), (4.25, 5.5), (6.75, 5.5),
                         (1.75, 2)  , (4.25, 2)  , (6.75, 2)],
@@ -26,7 +29,8 @@ POKER_L = {'card_dim' : (3.5*inch, 2.5*inch),
 POKER_L['anchors'] = [(inch*a[0], inch*a[1]) for a in POKER_L['anchors']]
 
 # 4x2 poker cards on 8.5"x11" paper
-POKER_8 = {'card_dim' : (2.5*inch, 3.5*inch),
+POKER_8 = {'page' : letter,
+           'card_dim' : (2.5*inch, 3.5*inch),
            'anchors' : [(2.5, 9.25), (6, 9.25),
                         (2.5, 6.75), (6, 6.75),
                         (2.5, 4.25), (6, 4.25),
@@ -34,38 +38,44 @@ POKER_8 = {'card_dim' : (2.5*inch, 3.5*inch),
            'card_rots' : [-90, 90, -90, 90, -90, 90, -90, 90]}
 POKER_8['anchors'] = [(inch*a[0], inch*a[1]) for a in POKER_8['anchors']]
 
-BRIDGE = {'card_dim' : (2.25*inch, 3.5*inch),
+BRIDGE = {'page' : letter,
+          'card_dim' : (2.25*inch, 3.5*inch),
           'anchors' : [(2, 9)  , (4.25, 9)  , (6.5, 9),
                        (2, 5.5), (4.25, 5.5), (6.5, 5.5),
                        (2, 2)  , (4.25, 2)  , (6.5, 2)],
           'card_rots' : [0,0,0,0,0,0,0,0,0]}
 BRIDGE['anchors'] = [(inch*a[0], inch*a[1]) for a in BRIDGE['anchors']]
 
-TAROT = {'card_dim' : (2.75*inch, 4.75*inch),
+TAROT = {'page' : letter,
+         'card_dim' : (2.75*inch, 4.75*inch),
          'anchors' : [(1.875, 7.875), (1.875, 3.125),
                       (5.625, 8.25), (5.625, 5.5), (5.625, 2.75)],
          'card_rots' : [0, 0, 90, 90, 90]}
 TAROT['anchors'] = [(inch*a[0], inch*a[1]) for a in TAROT['anchors']]
 
-JUMBO = {'card_dim' : (3.34090909*inch, 5.25*inch),
+JUMBO = {'page' : letter,
+         'card_dim' : (3.34090909*inch, 5.25*inch),
          'anchors' : [(2.5795455, 8.125), (5.9204546, 8.125),
                       (2.5795455, 2.875), (5.9204546, 2.875)],
          'card_rots' : [0, 0, 0, 0]}
 JUMBO['anchors'] = [(inch*a[0], inch*a[1]) for a in JUMBO['anchors']]
 
-JUMBO_L = {'card_dim' : (3.34090909*inch, 5.25*inch),
+JUMBO_L = {'page' : letter,
+           'card_dim' : (3.34090909*inch, 5.25*inch),
            'anchors' : [(2.5795455, 8.125), (5.9204546, 8.125),
                         (2.5795455, 2.875), (5.9204546, 2.875)],
            'card_rots' : [90, 90, 90, 90]}
 JUMBO_L['anchors'] = [(inch*a[0], inch*a[1]) for a in JUMBO_L['anchors']]
 
-SQUARE_4_INCH = {'card_dim' : (4*inch, 4*inch),
+SQUARE_4_INCH = {'page' : letter,
+                'card_dim' : (4*inch, 4*inch),
                 'anchors' : [(2.25, 7.5), (6.25, 7.5),
                              (2.25, 3.5), (6.25, 3.5)],
                 'card_rots' : [0,0,0,0]}
 SQUARE_4_INCH['anchors'] = [(inch*a[0], inch*a[1]) for a in SQUARE_4_INCH['anchors']]
 
 SQUARE_2_INCH = {
+    'page' : letter,
     'card_dim' : (2*inch, 2*inch),
     'anchors' : [(1.25,9.5), (3.25,9.5), (5.25,9.5), (7.25,9.5),
                  (1.25,7.5), (3.25,7.5), (5.25,7.5), (7.25,7.5),
@@ -75,12 +85,18 @@ SQUARE_2_INCH = {
     'card_rots' : [0]*20}
 SQUARE_2_INCH['anchors'] = [(inch*a[0], inch*a[1]) for a in SQUARE_2_INCH['anchors']]
 
-SQUARE_2_5_INCH = {'card_dim' : (2.5*inch, 2.5*inch),
+SQUARE_2_5_INCH = {'page' : letter,
+           'card_dim' : (2.5*inch, 2.5*inch),
            'anchors' : [(1.75, 9)  , (4.25, 9)  , (6.75, 9),
                         (1.75, 5.5), (4.25, 5.5), (6.75, 5.5),
                         (1.75, 2)  , (4.25, 2)  , (6.75, 2)],
            'card_rots' : [0,0,0,0,0,0,0,0,0]}
 SQUARE_2_5_INCH['anchors'] = [(inch*a[0], inch*a[1]) for a in SQUARE_2_5_INCH['anchors']]
+
+ICF_OBLIQUE = {'page' : (191.304, 263.304),
+        'card_dim' : (2.657*inch, 3.657*inch),
+        'anchors' : [(1.3285*inch, 1.8285*inch)],
+        'card_rots' : [0]}
 
 def read_specs(spec_filename):
     with open(spec_filename, 'r') as csvfile:
@@ -144,7 +160,7 @@ def draw_cards(card_list, filename, layout):
     cards_per_sheet = len(layout['anchors'])
     num_pages = math.ceil(num_cards / cards_per_sheet)
 
-    c = canvas.Canvas(filename, pagesize=letter)
+    c = canvas.Canvas(filename, pagesize=layout['page'])
     c.setStrokeColorRGB(0,0,0)
     for i in range(num_pages):
         for j in range(cards_per_sheet):
@@ -166,16 +182,15 @@ class Card(object):
         self.rect_list = rect_list
 
     def draw(self, c, anchor, rot):
-        c.saveState()
-
         # translate and rotate
+        c.saveState() # to draw card
         c.translate(*anchor)
         c.rotate(rot)
         c.translate(*[-x/2 for x in self.dim])
 
         # draw background image
         if self.bg:
-            c.saveState()
+            c.saveState() # to draw background image
             p = c.beginPath()
             p.rect(0,0,*self.dim)
             c.clipPath(p, stroke=0)
@@ -185,7 +200,7 @@ class Card(object):
                         self.dim[0]*self.bg.x_scale,
                         self.dim[1]*self.bg.y_scale,
                         mask='auto')
-            c.restoreState()
+            c.restoreState() # background image complete
             
         # draw lines
         for line in self.line_list:
@@ -198,12 +213,23 @@ class Card(object):
             print(rect_abs)
             c.rect(*rect_abs)
 
+        # draw text sections
+        registered_fonts = c.getAvailableFonts()
+        needed_fonts = list(set([s.font_name for s in self.section_list]))
+        for f in needed_fonts:
+            if f not in registered_fonts:
+                pdfmetrics.registerFont(TTFont(f, '{0}.ttf'.format(f)))
         for section in self.section_list:
+            c.saveState() # to draw section
+            c.translate(*anchor)
+            c.rotate(section.rot)
+            c.translate(*[-x/2 for x in self.dim])
+
             font_size = section.font_size
             font_name = section.font_name
             textobject = c.beginText()
             textobject.setFont(font_name, font_size)
-            textobject.setFillColorRGB(0.35, 0.35, 0.45) # cool dark grey
+            textobject.setFillColorRGB(*section.font_color)
             text_posn_abs = [d*t for d,t in zip(self.dim, section.posn)]
             textobject.setTextOrigin(*text_posn_abs)
 
@@ -235,7 +261,9 @@ class Card(object):
 
             c.drawText(textobject)
 
-        c.restoreState()
+            c.restoreState() # section complete
+
+        c.restoreState() # card complete
 
 
 class Section(object):
@@ -243,8 +271,10 @@ class Section(object):
         self.alignment = 'bottomleft'
         self.font_size = 10
         self.font_name = 'Helvetica'
+        self.font_color = (0,0,0)
         self.posn = spec[:2]
-        self.wrap_width = spec[2]
+        self.rot = spec[2]
+        self.wrap_width = spec[3]
 
         self.text_list = text.split('\n')
         self.text_list = [textwrap.wrap(t, self.wrap_width)
@@ -254,12 +284,14 @@ class Section(object):
         self.text_list = [item for sublist in self.text_list
                           for item in sublist]
 
-        if len(spec) > 3:
-            self.alignment = spec[3].lower()
         if len(spec) > 4:
-            self.font_size = spec[4]
+            self.alignment = spec[4].lower()
         if len(spec) > 5:
-            self.font_name = spec[5]
+            self.font_size = spec[5]
+        if len(spec) > 6:
+            self.font_name = spec[6]
+        if len(spec) > 7:
+            self.font_color = spec[7]
 
 
 class Background(object):
